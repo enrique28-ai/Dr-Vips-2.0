0d756a0d334dad85c25ea00e6debe15bcfad7798
Enrique Preciado <enriqueprecia@gmail.com>
2025-09-29 03:22:38 -0700

    chore: update gmail transporter config

--- backend/src/utils/gmail.js
+++ backend/src/utils/gmail.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,       // secure SSL
+  port: 587,       // STARTTLS
   secure: false,
   requireTLS: true,
   auth: {
